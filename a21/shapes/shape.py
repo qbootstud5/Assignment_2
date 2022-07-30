96d9ec8b1d0528dce69db05ed7e6633b22b75f08
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-07-30 13:13:04 +0200

    Make a21 a package

---
+++ a21/shapes/shape.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+from ..color import Color
+from ..html_doct import HtmlDoc
+from ..point import Point
+
+
+class Shape(ABC):
+    def __init__(self, color: Color, position: Point, opacity: float) -> None:
+        self._color = color
+        self._position = position
+        self._opacity = opacity
+
+    def write_shape_line(self, hd: HtmlDoc) -> None:
+        shape_string: str = self.line_header() + self.position_string() + self.characteristic_string() + \
+                            self.color_string() + self.line_tail()
+        hd.write_html_line(2, shape_string)
+
+    @abstractmethod
+    def line_header(self) -> str:
+        ...
+
+    @abstractmethod
+    def line_tail(self) -> str:
+        ...
+
+    @abstractmethod
+    def characteristic_string(self) -> str:
+        ...
+
+    def color_string(self) -> str:
+        return f'fill=rgb({self._color.red}, {self._color.green}, {self._color.blue})" fill-opacity="{self._opacity}">'
+
+    def position_string(self) -> str:
+        return f'cx="{self._position.x}" cy="{self._position.y}"'

50bbae3a5f03d2e52c4163056a64e2bbff4ee16d
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-07-30 11:54:15 +0200

    first draft of svg canvas class. Only write head and tail generation

---
+++ a21/svg_canvas.py
@@ -0,0 +1,15 @@
+from html_doct import HtmlDoc
+
+
+class SvgCanvas:
+    def __init__(self, width: int, height: int) -> None:
+        self.__width: int = width
+        self.__height: int = height
+
+    def write_svg_header(self, hd: HtmlDoc) -> None:
+        hd.write_html_line(1, f'<svg width="{self.__width}" height="{self.__height}"')
+
+    def write_svg_tail(self, hd: HtmlDoc) -> None:
+        hd.write_html_line(1, "/svg")
+
+

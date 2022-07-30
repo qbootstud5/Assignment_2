20e6fcf7b158c945955983f6218dab2212a592c2
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-07-30 12:03:11 +0200

    Correct the strings in the head and tail writer

--- a21/svg_canvas.py
+++ a21/svg_canvas.py
@@ -7,9 +7,7 @@ class SvgCanvas:
         self.__height: int = height
 
     def write_svg_header(self, hd: HtmlDoc) -> None:
-        hd.write_html_line(1, f'<svg width="{self.__width}" height="{self.__height}"')
+        hd.write_html_line(1, f'<svg width="{self.__width}" height="{self.__height}">')
 
     def write_svg_tail(self, hd: HtmlDoc) -> None:
-        hd.write_html_line(1, "/svg")
-
-
+        hd.write_html_line(1, "</svg>")

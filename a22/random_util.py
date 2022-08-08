1d3712a022b30a02f517d7c2f9dacd7570a8420f
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-08-08 23:24:43 +0200

    rewrite the random according to new instructions. Made it simpler

--- a22/random_util.py
+++ a22/random_util.py
@@ -1,75 +1,28 @@
 import random
-from typing import Tuple
-from a22.art_config import ArtConfig
+from typing import Union
 
 
-class RandomUtil:
-    # Default ranges
-    x_range: Tuple[int, int]
-    y_range: Tuple[int, int]
-    # For colors
-    red_range: Tuple[int, int]
-    green_range: Tuple[int, int]
-    blue_range: Tuple[int, int]
-
-    # For circle radius
-    circ_radius_range: Tuple[int, int]
-
-    # For ellipse radiuses
-    ellipse_rx_range: Tuple[int, int]
-    ellipse_ry_range: Tuple[int, int]
-
-    # Rectangle_width
-    rect_width_range: Tuple[int, int]
-    rect_height_range: Tuple[int, int]
-
-    # Opacity
-    opacity_range: Tuple[float, float]
-
-    def __init__(self, art_config: ArtConfig) -> None:
-        self.x_range = art_config.x_range
-        self.y_range = art_config.y_range
-        self.red_range = art_config.red_range
-        self.green_range = art_config.green_range
-        self.blue_range = art_config.blue_range
-        self.circ_radius_range = art_config.circ_radius_range
-        self.ellipse_rx_range = art_config.ellipse_rx_range
-        self.ellipse_ry_range = art_config.ellipse_ry_range
-        self.rect_width_range = art_config.rect_width_range
-        self.rect_height_range = art_config.rect_height_range
-        self.opacity_range = art_config.opacity_range
+class RandomUtil2:
+    @classmethod
+    def gen_int_in_range(cls, min_value: Union[int, float], max_value: Union[int, float]) -> int:
+        if min_value.is_integer():
+            raise TypeError(f"The min_value should be an integer, not {min_value}")
+        if max_value.is_integer():
+            raise TypeError(f"The max_value should be an integer, not {max_value}")
+        return random.randint(min_value, max_value)
 
     @classmethod
-    def gen_int_in_range(cls, variable_name: str) -> int:
-        if variable_name not in cls.int_correspondency_table().keys():
-            raise ValueError("The chosen feature is not implemented for int generation")
-        return random.randint(*cls.int_correspondency_table()[variable_name])
+    def gen_float_in_range(cls, min_value: Union[int, float], max_value: Union[int, float]) -> float:
+        try:
+            float_min = float(min_value)
+        except ValueError:
+            raise ValueError("The given min_value cannot be converted to float")
 
-    @classmethod
-    def gen_float_in_range(cls, variable_name: str) -> float:
-        if variable_name not in cls.float_correspondency_table().keys():
-            raise ValueError("The chosen feature is not implemented for float generation")
-        return random.uniform(*cls.float_correspondency_table()[variable_name])
+        try:
+            float_max = float(max_value)
+        except ValueError:
+            raise ValueError("The given max_value cannot be converted to float")
 
-    @classmethod
-    def int_correspondency_table(cls) -> dict:
-        correspondency_dict = {
-            "x": cls.x_range,
-            "y": cls.y_range,
-            "red": cls.red_range,
-            "green": cls.green_range,
-            "blue": cls.blue_range,
-            "circ_radius": cls.circ_radius_range,
-            "ellipse_rx": cls.ellipse_rx_range,
-            "ellipse_ry": cls.ellipse_ry_range,
-            "rect_width": cls.rect_width_range,
-            "rect_height": cls.rect_height_range
-        }
-        return correspondency_dict
+        return random.uniform(float_min, float_max)
 
-    @classmethod
-    def float_correspondency_table(cls) -> dict:
-        correspondency_dict = {
-            "opacity": cls.opacity_range
-        }
-        return correspondency_dict
+

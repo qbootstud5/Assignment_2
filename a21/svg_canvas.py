1f1ef6e9ab10e5644a18215fbcabc8d7463887d0
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-08-09 23:23:53 +0200

    add div to center image

--- a21/svg_canvas.py
+++ a21/svg_canvas.py
@@ -12,11 +12,13 @@ class SvgCanvas:
         self.__height: int = height
 
     def write_svg_header(self, hd: HtmlDoc) -> None:
+        hd.write_html_line(1,f'<div style="text-align:center;">')
         hd.write_html_line(1, f'<svg width="{self.__width}" height="{self.__height}">')
 
     @staticmethod
     def write_svg_tail(hd: HtmlDoc) -> None:
         hd.write_html_line(1, "</svg>")
+        hd.write_html_line(1, "</div>")
 
     @staticmethod
     def gen_art(hd: HtmlDoc, shape_list: List[Union[Circle, Rectangle, Ellipse]]) -> None:

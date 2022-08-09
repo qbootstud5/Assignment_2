f5078e2435e38dfac2dcbf2e4309abe5e0c7cb88
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2022-08-09 21:16:22 +0200

    made the methods static

--- a21/svg_canvas.py
+++ a21/svg_canvas.py
@@ -1,9 +1,9 @@
 from typing import Union, List
 
-from html_doct import HtmlDoc
-from shapes.circle import Circle
-from shapes.ellipse import Ellipse
-from shapes.rectangle import Rectangle
+from a21.html_doct import HtmlDoc
+from a21.shapes.circle import Circle
+from a21.shapes.ellipse import Ellipse
+from a21.shapes.rectangle import Rectangle
 
 
 class SvgCanvas:
@@ -14,9 +14,11 @@ class SvgCanvas:
     def write_svg_header(self, hd: HtmlDoc) -> None:
         hd.write_html_line(1, f'<svg width="{self.__width}" height="{self.__height}">')
 
-    def write_svg_tail(self, hd: HtmlDoc) -> None:
+    @staticmethod
+    def write_svg_tail(hd: HtmlDoc) -> None:
         hd.write_html_line(1, "</svg>")
 
-    def gen_art(self, hd: HtmlDoc, shape_list: List[Union[Circle, Rectangle, Ellipse]]) -> None:
+    @staticmethod
+    def gen_art(hd: HtmlDoc, shape_list: List[Union[Circle, Rectangle, Ellipse]]) -> None:
         for shape in shape_list:
             shape.write_shape_line(hd)
